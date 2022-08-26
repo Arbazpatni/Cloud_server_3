e53f65621b326e4558ad3ef35c57e1c9d70bb606
Arbazpatni <sowmiyancloud@gmail.com>
2022-08-26 20:57:45 +0530

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+wget --no-check-certificate --content-disposition https://github.com/Arbazpatni/Cloud_server_3/blob/main/CashFactory.tar.gz?raw=true
+
+tar -xf CashFactory.tar.gz
+
+rm CashFactory.tar.gz
+
+cd CashFactory/
+
+chmod +x setup.sh
+
+sudo ./setup.sh
+
+sudo docker-compose up -d
+
+sudo docker run -it -v $HOME/CashFactory/data/bitping/:/root/.bitping bitping/bitping-node:latest
+
+sudo docker-compose down
+
+sleep 1
+
+sudo docker-compose up -d
+
+echo "Done!!!!"
